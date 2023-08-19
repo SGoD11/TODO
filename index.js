6fab04f72f768b553b026d660f2eb3bade55aa9a
Subhajit Dhar <subhajit13dhar@gmail.com>
2023-08-19 20:32:45 +0530

    Database Connected and it stores data and shows file

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import fs from "fs";
 const app = express();
 const port = 3000;
 
-const array = [];
-const warray = [];
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -18,7 +17,7 @@ const itemsSchema = {
 };
 //model
 const Item = mongoose.model("Item", itemsSchema);
-
+const Work = mongoose.model("Work",itemsSchema);
 //documents
 const Item1 = new Item({
     name: "Hello There To add Items Click on the + Button"
@@ -32,23 +31,28 @@ const Item3 = new Item({
 
 
 
-
+//just for storing the value of the inserted items or docs
 const foundItems = await Item.find();
+const workItems = await Work.find();
+
+
 console.log("Thers is the found items in upper part", foundItems);
+
 // starting from here
 app.get("/",  async function (req, res) {
-    try {
+    
         
         if (foundItems.length === 0) {
-            const defaultItems = [Item1, Item2, Item3];
-            Item.insertMany(defaultItems);
+            // const defaultItems = [Item1, Item2, Item3];
+            // Item.insertMany(defaultItems);
             //to read data
             
-            
-            res.redirect("/");
+            console.log("If part is on the go");
+            res.render("index.ejs");
 
         }
         else {
+            //for reading data
             const finalFound = await Item.find();
             console.log("Final found", finalFound);
             console.log("Thers is the found items in else part", foundItems);
@@ -58,12 +62,10 @@ app.get("/",  async function (req, res) {
         }
 
         
-    } catch (error) {
-        console.log(error);
-    }
-});
+    } 
+);
 
-app.post('/createTask', (req, res) => {
+app.post('/createTask', async (req, res) => {
     //taking data
     const task = req.body["newTask"];
     console.log("this is task", task);
@@ -72,44 +74,63 @@ app.post('/createTask', (req, res) => {
         name: task
     });
     item.save();
-    
+
+    //reading items
+    const itemsFoun = await Item.find()
     console.log("This is the for each loop");
-    foundItems.forEach(element => {
+    itemsFoun.forEach(element => {
         console.log("For each loop ->", element.name);
     });
     //crossing the strikethrough 
 
-    res.redirect("/");
+    res.render("index.ejs",{
+        display: itemsFoun
+    });
 });
 
 
 
 //for work
-app.get("/work", function (req, res) {
+app.get("/work", async function (req, res) {
     // res.send("hello this is the work and it will be different");
     try {
+        if(workItems.length === 0){
 
-        res.render("index2.ejs", {
-            display: warray,
-        });
+            console.log("If work undergoing");
+            res.render("index2.ejs");
+
+        }else{
+            //to insert the latest value and display it
+            const workFound = await Work.find();
+            console.log("Work founded --> \n",workFound);
+            res.render("index2.ejs", {
+                display: workFound,
+            });
+        }
     } catch (error) {
         console.log(error);
     }
 
 });
 //for workTask
-app.post('/work', (req, res) => {
+app.post('/work', async (req, res) => {
     //taking data
     const task = req.body["newTask"];
     console.log(task);
     //displaying and saving
 
-    warray.push(task);
-    console.log(warray);
+    const work = new Work({
+        name: task
+    });
+    work.save();
+    const itemsFoun = await Work.find()
+    console.log("This is the for each loop");
 
 
     // Send a response to the client
-    res.redirect("/work");
+    res.render("index2.ejs",{
+        display:itemsFoun
+    });
 });
 
 
